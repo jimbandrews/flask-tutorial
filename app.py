1d950d02e372a535591a763a3eaa6189e257dfc3
James Andrews <jbandrew14@gmail.com>
2022-02-27 17:22:04 -0500

    added example dashboard and login routes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -11,5 +12,18 @@ def index():
 @app.route('/hello')
 def hello_world():
     return "hello world"
+
+@app.route('/dashboard/<name>')
+def dashboard(name):
+    return f"welcome {name}"
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['name']
+        return redirect(url_for('dashboard', name=user))
+    else:
+        user = request.args.get('name')
+        return render_template('login.html')
 
 app.run(host='0.0.0.0', port=81)
